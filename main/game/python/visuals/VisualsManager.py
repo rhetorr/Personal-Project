97f86054f93d6e14e229e46b1aad4b432c3f284d
rhetorr <140564619+rhetorr@users.noreply.github.com>
2025-02-22 22:06:32 -0800

    make settings work (fullscreen so far)

--- main/game/python/visuals/VisualsManager.py
+++ main/game/python/visuals/VisualsManager.py
@@ -16,28 +16,34 @@ class VisualsManager:
     def __init__(self, resolution, caption: str, icon_filename: str, mouse: Mouse):
         self.config_settings = Settings.get_config_dict()
         
+        self.full_res = (get_monitors()[self.config_settings["main_monitor"]].width,get_monitors()[self.config_settings["main_monitor"]].height)
+        self.def_res = resolution
+        self.used_res = self.full_res if self.config_settings["fullscreen"] else resolution
+        
+        self.res_scalar = Point(self.used_res[0]/self.def_res[0], self.used_res[1]/self.def_res[1])
+        
         self.mouse = mouse
         self.i_h = ImageHelpers(VisualsUtil._ASSETS_PATH)
         
-        self._window_ = pygame.display.set_mode((get_monitors()[self.config_settings["main_monitor"]].width,get_monitors()[self.config_settings["main_monitor"]].height) if self.config_settings["fullscreen"] else resolution, vsync=1) #creating window
+        self._window_ = pygame.display.set_mode(self.used_res, vsync=1) #creating window
         pygame.display.set_caption(caption)
-        self.icon = Sprite(self._window_, Point.fill(100)).with_image(icon_filename)
+        self.icon = Sprite(self._window_, self.res_scalar.times(Point.fill(100))).with_image(icon_filename)
         pygame.display.set_icon(self.icon.sprite)
         
         
-        self.font = TextHelpers(self._window_, "arial", 20)
-        self.settings_font = TextHelpers(self._window_, "times new roman", 40)
+        self.font = TextHelpers(self._window_, "arial", round(20*self.res_scalar.x))
+        self.settings_font = TextHelpers(self._window_, "times new roman", round(40*self.res_scalar.x))
         
-        self.back_button = Button(self._window_, Point.fill(0), Point.fill(75), "<", 30, bg_hover="red")
+        self.back_button = Button(self._window_, Point.fill(0), self.res_scalar.times(Point.fill(75)), "<", round(30*self.res_scalar.x), bg_hover="red")
         
-        self.icon_menu = Sprite(self._window_, Point.fill(225)).with_image(icon_filename)
-        self.play_button = Button(self._window_, Point.fill(0), Point(250, 75), "Play", 30, "sky blue", "blue")
-        self.settings_button = Button(self._window_, Point.fill(0), Point(250, 75), "Settings", 30)
-        self.quit_button = Button(self._window_, Point.fill(0), Point(250, 75), "Quit", 30, bg_hover="red")
+        self.icon_menu = Sprite(self._window_, self.res_scalar.times(Point.fill(225))).with_image(icon_filename)
+        self.play_button = Button(self._window_, Point.fill(0), Point(250, 75).times(self.res_scalar), "Play", round(30*self.res_scalar.x), "sky blue", "blue")
+        self.settings_button = Button(self._window_, Point.fill(0), Point(250, 75).times(self.res_scalar), "Settings", round(30*self.res_scalar.x))
+        self.quit_button = Button(self._window_, Point.fill(0), Point(250, 75).times(self.res_scalar), "Quit", round(30*self.res_scalar.x), bg_hover="red")
         
-        self.settings_panel = RectHelpers(Point(100, 0), Point(self._window_.get_width()-200, self._window_.get_height()))
+        self.settings_panel = RectHelpers(Point(100, 0).times(self.res_scalar), Point(self._window_.get_width(), self._window_.get_height()).minus(Point(200, 0).times(self.res_scalar)))
         self.txt_fullscreen = self.settings_font.make_text("Fullscreen", "black")
-        self.fullscreen_button = Button(self._window_, Point.fill(0), Point.fill(75), "", 30, bg_hover="gray")
+        self.fullscreen_button = Button(self._window_, Point.fill(0), Point.fill(75).times(self.res_scalar), "", round(30*self.res_scalar.x), bg_hover="gray")
         
     def render(self, surface, coords):
         return self._window_.blit(surface, coords)
@@ -63,19 +69,19 @@ class VisualsManager:
                 self.font.full_render("LOADING...", "black", Point._key())
             case GameStates.MENU:
                 self.icon_menu.at(Point(self._window_.get_width()/2-self.icon_menu.size.x/2, 0)).show().render()
-                self.play_button.at(Point(self._window_.get_width()/2-self.play_button.size.x/2, self._window_.get_height()/2-self.play_button.size.y/2 - 100)).show().render(self.mouse)
+                self.play_button.at(Point(self._window_.get_width()/2-self.play_button.size.x/2, self._window_.get_height()/2-self.play_button.size.y/2).minus(Point(0, 100).times(self.res_scalar))).show().render(self.mouse)
                 self.settings_button.at(Point(self._window_.get_width()/2-self.settings_button.size.x/2, self._window_.get_height()/2)).show().render(self.mouse)
-                self.quit_button.at(Point(self._window_.get_width()/2-self.quit_button.size.x/2, self._window_.get_height()/2-self.quit_button.size.y/2 + 200)).show().render(self.mouse)
+                self.quit_button.at(Point(self._window_.get_width()/2-self.quit_button.size.x/2, self._window_.get_height()/2-self.quit_button.size.y/2).plus(Point(0,200).times(self.res_scalar))).show().render(self.mouse)
             case GameStates.SETTINGS:
                 self.settings_panel.render(self._window_, "gray")
-                self.back_button.at(Point(15,15)).show().render(self.mouse)
+                self.back_button.at(Point(15,15).times(self.res_scalar)).show().render(self.mouse)
                 
-                self.settings_font.render(self.txt_fullscreen, Point(125, 25))
+                self.settings_font.render(self.txt_fullscreen, Point(125, 25).times(self.res_scalar))
                 if self.config_settings["fullscreen"]:
                     self.fullscreen_button.with_image("checkmark.png")
                 else:
                     self.fullscreen_button.with_text("")
-                self.fullscreen_button.at(Point(125+self.txt_fullscreen.get_width()/2-self.fullscreen_button.size.x/2, 25+self.txt_fullscreen.get_height())).show().render(self.mouse)
+                self.fullscreen_button.at(Point(self.txt_fullscreen.get_width()/2-self.fullscreen_button.size.x/2,self.txt_fullscreen.get_height()).plus(Point(125,25).times(self.res_scalar))).show().render(self.mouse)
                 
             case GameStates.STARTING | GameStates.PLAYING | GameStates.LOST:
                 player.show().render()
