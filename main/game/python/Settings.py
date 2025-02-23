33544c9d3d98902bb038d291359e7e1fd77d1291
rhetorr <140564619+rhetorr@users.noreply.github.com>
2025-02-22 22:08:03 -0800

    make the settings path inside appdata

--- main/game/python/Settings.py
+++ main/game/python/Settings.py
@@ -13,11 +13,13 @@ _SETTINGS_PATH = 'C:/Users/' + _USER + '/AppData/Local/PersonalProject/' + _SETT
 def save_settings(settings: dict):
     file = File()
     file.make(_SETTINGS_FILE_NAME)
+    file.__file_path = _SETTINGS_PATH
     file.write([str(settings)])
     
 def get_config_dict() -> dict:
     settings = File()
     settings.make(_SETTINGS_FILE_NAME)
+    settings.__file_path = _SETTINGS_PATH
     if not settings.read():
         return _EMPTY_SETTINGS
     return eval(settings.read())
