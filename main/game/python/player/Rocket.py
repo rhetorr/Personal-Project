7fa00e815a4a41cb4123fd6f6e718433c1a00fab
rhetorr <140564619+rhetorr@users.noreply.github.com>
2025-02-24 23:28:36 -0800

    completely remake rocket class

--- main/game/python/player/Rocket.py
+++ main/game/python/player/Rocket.py
@@ -1,18 +1,25 @@
 import pygame
+from util.mathextra.Location import Angle, Orientation, Point
+from visuals import VisualsUtil
+from util.ImageHelpers import ImageHelpers
 
-from util.mathextra.Location import Point
-from visuals.Sprite import Sprite
-from GameStates import GameStates
-
-class Rocket(Sprite):
-    def __init__(self, window: pygame.Surface):
-        super().__init__(window, Point.fill(75))
-        self.with_image("rocket.png")
-        self.appear_states = (GameStates.PLAYING, GameStates.LOST, GameStates.STARTING)
-    def update(self, state: GameStates):
-        for i in self.appear_states:
-            if i == state:
-                self.show()
-            else:
-                self.hide()
-        self.render()
+class Rocket():
+    def __init__(self, window:pygame.Surface, size: Point, res_scalar: Point):
+        self.size = size.times(res_scalar)
+        self.__window__ = window
+        self.i_h = ImageHelpers(VisualsUtil._ASSETS_PATH)
+        
+        self.rocket = self.i_h.resize(self.i_h.get("rocket.png"), self.size)
+        self.__rocket_angle__ = Angle.in_degrees(0)
+        
+        self.pos: Orientation = Orientation.init(size.negate().minus(Point.fill(100)), Angle.in_degrees(0))
+    def render(self):
+        self.rocket = self.i_h.rotate(self.rocket, self.pos.angle.minus(self.__rocket_angle__))
+        self.__rocket_angle__ = self.pos.angle
+        self.__window__.blit(self.rocket, self.pos.get_point().tuple())
+    def at(self, pos: Point):
+        self.pos = Orientation.init(pos, self.pos.angle)
+        return self
+    def face(self, angle: Angle):
+        self.pos.angle = angle
+        return self
